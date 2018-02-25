69f9e0a0f82cc6d0ac53d97d76b8ff2035555de8
hvdson <hvdsonmusic@gmail.com>
2018-02-25 19:41:30 +0000

    Finished exercise 13

--- exercise_13.rb
+++ exercise_13.rb
@@ -1,1 +1,14 @@
-ls
+first, second, third = ARGV
+
+puts "First variable is: #{first}"
+puts "Second variable is: #{second}"
+puts "Third variable is: #{third}"
+
+print "Give me something dank: "
+dank = STDIN.gets.chomp
+
+puts "Dank variable is: #{dank}"
+
+print "Give me a memes: "
+memes = STDIN.gets.chomp
+puts "Memes variable is: #{memes}"
